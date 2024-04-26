007fabdc1ad061945e2858dc7e921889e3c47c72
Edson de Oliveira Correia <118702317+jovem-edson@users.noreply.github.com>
2024-04-26 14:28:26 +0000

    cadastrarCliente

--- codigo/ManterCliente.java
+++ codigo/ManterCliente.java
@@ -1,11 +1,20 @@
 package codigo;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 public class ManterCliente {
   list<Cliente> clientes = new ArrayList<>();
   
-  public void cadastrarCliente(Cliente cliente) {
+  public void cadastrarCliente() {
     Cliente cliente = new Cliente();
     cliente.setNomeCliente(JOptionPane.showInputDialog("Digite o nome do cliente: "));
+    cliente.setCpfCliente(JOptionPane.showInputDialog("Digite o CPF do cliente: "));
+    cliente.setTelefoneCliente(JOptionPane.showInputDialog("Digite o telefone do cliente: "));
+    cliente.setEmailCliente(JOptionPane.showInputDialog("Digite o email do cliente: "));
+
+    clientes.add(cliente);
 
   }
 
